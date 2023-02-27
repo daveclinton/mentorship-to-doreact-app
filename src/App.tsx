cdaee9b67e84c9c35fccad08da037a422b448f98
daveclinton <clintondavid46@gmail.com>
2023-02-28 00:49:18 +0300

    Add ToDoList component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
 import * as React from "react";
-import { Box } from "@chakra-ui/react";
+import { Flex, Box } from "@chakra-ui/react";
+import ToDoList from "./components/ToDoList";
 
 const App: React.FC = () => {
-  return <Box bg="red">Hello</Box>;
+  return (
+    <Flex justifyContent="center" h="100vh" alignItems="center">
+      <Box boxShadow="0px 2px 20px rgba(0, 0, 0, 0.1)" boxSize="700px">
+        <ToDoList />
+      </Box>
+    </Flex>
+  );
 };
 
 export default App;
---
+++ src/components/ToDoList.tsx
@@ -0,0 +1,42 @@
+import * as React from "react";
+import { Box, FormControl, Input, Button, Flex, Text } from "@chakra-ui/react";
+
+const ToDoList: React.FC = () => {
+  const [task, setTask] = React.useState("");
+  const [addedTask, setAddedTask] = React.useState<string[]>([]);
+
+  const handleInputChange = (input: any) => setTask(input.target.value);
+
+  const handleAddTask = () => {
+    setAddedTask([...addedTask, task]);
+    setTask;
+  };
+
+  return (
+    <Box p="20px">
+      <Flex>
+        <FormControl display="flex" gap="20px">
+          <Input type="Enter Plan" value={task} onChange={handleInputChange} />
+          <Button type="submit" onClick={handleAddTask}>
+            Add
+          </Button>
+        </FormControl>
+      </Flex>
+      <Box boxShadow="0px 2px 20px rgba(0, 0, 0, 0.1)">
+        {addedTask.map((task, index) => (
+          <Flex
+            boxShadow="0px 2px 20px rgba(0, 0, 0, 0.1)"
+            key={index}
+            justifyContent="space-between"
+            mt="10px"
+          >
+            <Text m="auto">{task}</Text>
+            <Button>Delete</Button>
+          </Flex>
+        ))}
+      </Box>
+    </Box>
+  );
+};
+
+export default ToDoList;
