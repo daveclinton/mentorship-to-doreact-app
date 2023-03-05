1958b1eb11216a4b2cb8ba3dedf5e6e19f47e5bf
daveclinton <clintondavid46@gmail.com>
2023-03-05 09:44:41 +0300

    Add to do box

--- src/components/ToDo/index.tsx
+++ src/components/ToDo/index.tsx
@@ -1,7 +1,22 @@
-import React from "react";
+import * as React from "react";
+import { Box, Flex } from "@chakra-ui/layout";
 
-const ToDoList = () => {
-  return <div>ToDoList</div>;
+const ToDoList: React.FC = () => {
+  return (
+    <Flex
+      w="50%"
+      mt="20px"
+      bg="#f3f3f4"
+      justify="center"
+      mx="auto"
+      h="500px"
+      align="center"
+    >
+      <Box textAlign="center" bg="#5b75e3" w="90%" p="8px" h="400px">
+        <Box bg="#fff">Hello</Box>
+      </Box>
+    </Flex>
+  );
 };
 
 export default ToDoList;
